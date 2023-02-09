996402595e161a5b17e66f9ed7299bce6d8d4f05
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-09 21:47:23 +0100

    feat(Controller CRUD functions): Added the controller CRUD functions with their corresponding method decorator

--- src/Movies/movies.controller.ts
+++ src/Movies/movies.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
 import { MoviesService } from "./movies.service";
 
 @Controller('movies')
 export class MoviesController {
     constructor(private moviesService: MoviesService) {}
+
+    @Post()
+    addMovie() {
+        const movie = this.moviesService.addMovie();
+    }
+
+    @Get()
+    getMovies() {
+        const movies = this.moviesService.getMovies();
+    }
+    
+    @Get()
+    getMovieById() {
+        const movie = this.moviesService.getMovieById();
+    }
+
+    @Patch()
+    updateMovieById() {
+        const movie = this.moviesService.updateMovieById();
+    }
+
+    @Delete()
+    deleteMovieById() {
+        const movie = this.moviesService.deleteMovieById();
+    }
 };
