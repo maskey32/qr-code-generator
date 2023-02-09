c2ba0fdf17547f72fd6a43f62d38f7f38a49444b
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-09 21:51:22 +0100

    feat(Controller function): Added the get qrcode controller function with the get decorator

--- src/Qrcode/qrcode.controller.ts
+++ src/Qrcode/qrcode.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { QrcodeService } from "./qrcode.service";
 
 @Controller('qrcode')
 export class QrcodeController {
     constructor(private qrcodeService: QrcodeService) {}
+
+    @Get()
+    getQrcode() {
+        const qrcode = this.qrcodeService.getQrcode();
+    }
 };
