88934f030e83a1aefca17f66b0786f68c6190262
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-09 18:56:44 +0100

    chore(Imports): Imported Movies and Qrcode modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MoviesModule } from './Movies/movies.module';
+import { QrcodeModule } from './Qrcode/qrcode.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule, QrcodeModule],
 })
 export class AppModule {}
