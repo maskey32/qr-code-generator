75c2d4d45999ad3f3968ff571da45b98ab7e9d68
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 04:44:19 +0100

    feat(Service logic): Added CRUD movie service logics

--- src/Movies/movies.service.ts
+++ src/Movies/movies.service.ts
@@ -1,14 +1,98 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/Prisma/prisma.service";
+import { Movie } from "@prisma/client";
+import { CreateMovieDto, UpdateMovieDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-@Injectable({})
+@Injectable()
 export class MoviesService {
-    addMovie() {}
+    constructor(private prisma: PrismaService) {};
 
-    getMovies() {}
+    addMovie(dto: CreateMovieDto): Promise<Movie> {
+        try {
+            const movie = this.prisma.movie.create({
+                data: { ...dto }
+            })
     
-    getMovieById() {}
+            return movie;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002')
+                    throw new ForbiddenException('Credentials taken');
+            }
 
-    updateMovieById() {}
+            throw error;
+        }
+    }
 
-    deleteMovieById() {}
+    async getMovies(): Promise<Movie[]> {
+        try {
+            const movies = await this.prisma.movie.findMany();
+    
+            const randomMovies = [];
+            for (let i = 0; i < 10; i++) {
+                const randomIndex = Math.floor(Math.random() * movies.length);
+                randomMovies.push(movies[randomIndex]);
+            }
+    
+            return randomMovies;  
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async getMovieById(movieId: number): Promise<Movie> {
+        try {
+            const movie = await this.prisma.movie.findUnique({
+                where: { id: movieId }
+            })
+
+            if (!movie) throw new ForbiddenException('Movie does not exist');
+
+            const retrievedMovie = await this.prisma.movie.findUnique({
+                where: { id: movieId }
+            })
+
+            return retrievedMovie;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateMovieById(movieId: number, dto: UpdateMovieDto): Promise<Movie> {
+        try {
+            const movie = await this.prisma.movie.findUnique({
+                where: { id: movieId }
+            })
+
+            if (!movie) throw new ForbiddenException('Movie does not exist');
+
+            const updatedMovie = await this.prisma.movie.update({
+                where: { id: movieId },
+                data: { ...dto }
+            })
+            
+            return updatedMovie;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteMovieById(movieId: number): Promise<Movie> {
+        try {
+            const movie = await this.prisma.movie.findUnique({
+                where: { id: movieId }
+            })
+
+            if (!movie) throw new ForbiddenException('Movie does not exist');
+
+            const deletedMovie = await this.prisma.movie.delete({
+                where: { id: movieId }
+            })
+
+            return deletedMovie;
+        } catch (error) {
+            throw error;
+        }
+    }
 };
