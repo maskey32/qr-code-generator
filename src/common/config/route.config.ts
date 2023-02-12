ce1bc89b820b4e01080ac9114e07beea274537eb
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 04:51:41 +0100

    chore(Route names): Created route names object and routev1 prefix string for endpoints

---
+++ src/common/config/route.config.ts
@@ -0,0 +1,7 @@
+export const RouteNames = {
+    API_DEFAULT: "/api",
+    API_V1: "v1",
+    MOVIES: "movies",
+};
+
+export const routesV1Prefix = `${RouteNames.API_DEFAULT}/${RouteNames.API_V1}`;
