63a7490a790c9c8abc8839a5d28e5d4688847050
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 04:43:27 +0100

    feat(Contoller logic): Added CRUD movie controller logics

--- src/Movies/movies.controller.ts
+++ src/Movies/movies.controller.ts
@@ -1,32 +1,61 @@
-import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { MoviesService } from "./movies.service";
+import { Movie } from "@prisma/client";
+import { CreateMovieDto, UpdateMovieDto } from "./dto";
+import { RouteNames, routesV1Prefix } from "../common/config";
 
-@Controller('movies')
+@Controller(`${routesV1Prefix}/${RouteNames.MOVIES}`)
 export class MoviesController {
     constructor(private moviesService: MoviesService) {}
 
+    @HttpCode(HttpStatus.CREATED)
     @Post()
-    addMovie() {
-        const movie = this.moviesService.addMovie();
+    async addMovie(@Body() dto: CreateMovieDto): Promise<{ message: string; data: Movie; }> {
+        const movie = await this.moviesService.addMovie(dto);
+
+        return {
+            message: 'Successfully created a movie',
+            data: movie
+        }
     }
 
     @Get()
-    getMovies() {
-        const movies = this.moviesService.getMovies();
+    async getMovies(): Promise<{ message: string; data: Movie[]; }> {
+        const movies = await this.moviesService.getMovies();
+
+        return {
+            message: 'Successfully fetched movies',
+            data: movies
+        }
     }
     
-    @Get()
-    getMovieById() {
-        const movie = this.moviesService.getMovieById();
+    @Get(':id')
+    async getMovieById(@Param('id', ParseIntPipe) movieId: number): Promise<{ message: string; data: Movie; }> {
+        const movie = await this.moviesService.getMovieById(movieId);
+
+        return {
+            message: 'Successfully fetched movie',
+            data: movie
+        }
     }
 
-    @Patch()
-    updateMovieById() {
-        const movie = this.moviesService.updateMovieById();
-    }
+    @Patch(':id')
+    async updateMovieById(@Param('id', ParseIntPipe) movieId: number, dto: UpdateMovieDto): Promise<{ message: string; data: Movie; }> {
+        const movie = await this.moviesService.updateMovieById(movieId, dto);
 
-    @Delete()
-    deleteMovieById() {
-        const movie = this.moviesService.deleteMovieById();
+        return {
+            message: 'Successfully updated movie',
+            data: movie
+        }
+    }
+    
+    @Delete(':id')
+    async deleteMovieById(@Param('id', ParseIntPipe) movieId: number): Promise<{ message: string; data: Movie; }> {
+        const movie = await this.moviesService.deleteMovieById(movieId);
+
+        return {
+            message: 'Successfully deleted movie',
+            data: movie
+        }
     }
 };
