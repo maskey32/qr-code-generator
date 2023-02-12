0644e162a8c2e5876a0e87744e398fdabee5ec3c
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 04:45:39 +0100

    feat(Export): Exported movie service globally

--- src/Movies/movies.module.ts
+++ src/Movies/movies.module.ts
@@ -1,9 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { MoviesController } from "./movies.controller";
 import { MoviesService } from "./movies.service";
 
+@Global()
 @Module({
     controllers: [MoviesController],
     providers: [MoviesService],
+    exports: [MoviesService]
 })
 export class MoviesModule {};
