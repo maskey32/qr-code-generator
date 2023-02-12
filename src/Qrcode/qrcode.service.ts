6ab1292ab9f0d1b73a6f8f062577a1639c6aacc3
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 04:34:30 +0100

    feat(Service logic): Added qrcode service logic

--- src/Qrcode/qrcode.service.ts
+++ src/Qrcode/qrcode.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import * as qrcode from 'qrcode';
+import { MoviesService } from "../Movies/movies.service";
 
-@Injectable({})
+@Injectable()
 export class QrcodeService {
-    getQrcode() {}
-};
+  constructor(private movieService: MoviesService) {}
+
+  async generateQRCode(data: string): Promise<string> {
+    const link = `https://chukwuma-qr-code-generator.netlify.app/movie?movies=${data}`;
+
+    return qrcode.toDataURL(link);
+  }
+
+  async getQrcode() {
+    const randomMovies = await this.movieService.getMovies();
+
+    const filteredArray = randomMovies.map(item => {
+        const { id, createdAt, updatedAt, ...filteredProperties } = item;
+        return filteredProperties;
+    });
+
+    const data = JSON.stringify(filteredArray);
+
+    return this.generateQRCode(data);
+  }
+}
